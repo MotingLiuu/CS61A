062d14f8612fc441e7f73fdcdff1116bd7991530
MotingLiuu <moting.liuu@gmail.com>
2024-12-23 15:41:47 +0900

    sequence

--- disc06/partition.py
+++ disc06/partition.py
@@ -1,12 +1,22 @@
-def partition(n: int, m: int):
-    partitionTree = []
-    def consTree(n: int, m: int, pos: list):
-        if n == m:
-            pos = 'True'
-        if n > m:
-            pos.append([])
-        else:
-            pos = 'False'
-        if m > 1:
-            consTree(n, m - 1, pos[1])
-            
+def partition(n, m):
+    if n == 0:
+        return True
+    if n < 0 or m <= 0:
+        return False
+    if n > 0:
+        left = partition(n - m, m)
+        right = partition(n, m - 1)
+        return [m, left, right]
+
+def printPartition(partitionTree, partition = []):
+    if partitionTree == True:
+        print(partition)
+    elif partitionTree == False:
+        return
+    else:
+        left = partitionTree[1]
+        right = partitionTree[2]
+        printPartition(left, partition + [partitionTree[0]])
+        printPartition(right, partition)
+
+printPartition(partition(6, 4))
