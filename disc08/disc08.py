cfc3759f05db3cc0fdfdf8929866acd0dcab04f3
MotingLiuu <moting.liuu@gmail.com>
2025-01-16 03:39:23 +0900

    completed disc08

---
+++ disc08/disc08.py
@@ -0,0 +1,166 @@
+def strange_loop():
+    '''return a loop link
+    
+    >>> s = strange_loop()
+    >>> s.rest.first.rest is s
+    True
+    
+    '''
+    link = Link(1)
+    link.first = link
+    link.rest = link
+    return link
+
+def sum_rec(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_rec(a, 2)
+    7
+    >>> sum_rec(a, 5)
+    15
+    >>> sum_rec(Link.empty, 1)
+    0
+    """
+    # Use a recursive call to sum_rec; don't call sum_iter
+    "*** YOUR CODE HERE ***"
+    if not isinstance(s, Link):
+        return 0
+    else:
+        if k > 0:
+            return s.first + sum_rec(s.rest, k - 1)
+        else:
+            return 0
+
+def sum_iter(s, k):
+    """Return the sum of the first k elements in s.
+
+    >>> a = Link(1, Link(6, Link(8)))
+    >>> sum_iter(a, 2)
+    7
+    >>> sum_iter(a, 5)
+    15
+    >>> sum_iter(Link.empty, 1)
+    0
+    """
+    # Don't call sum_rec or sum_iter
+    "*** YOUR CODE HERE ***"
+    sum = 0
+    while k > 0:
+        if isinstance(s, Link):
+            sum += s.first
+            s = s.rest
+        k -= 1
+    return sum
+    
+def overlap(s, t):
+    """For increasing s and t, count the numbers that appear in both.
+
+    >>> a = Link(3, Link(4, Link(6, Link(7, Link(9, Link(10))))))
+    >>> b = Link(1, Link(3, Link(5, Link(7, Link(8)))))
+    >>> overlap(a, b)  # 3 and 7
+    2
+    >>> overlap(a.rest, b)  # just 7
+    1
+    >>> overlap(Link(0, a), Link(0, b))
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    if not (isinstance(s, Link) and isinstance(t, Link)):
+        return 0
+    if s.first == t.first:
+        return 1 + overlap(s.rest, t.rest)
+    elif s.first < t.first:
+        return overlap(s.rest, t)
+    elif s.first > t.first:
+        return overlap(s, t.rest)
+    
+def display(s, k=10):
+    """Print the first k digits of infinite linked list s as a decimal.
+
+    >>> s = Link(0, Link(8, Link(3)))
+    >>> s.rest.rest.rest = s.rest.rest
+    >>> display(s)
+    0.8333333333...
+    """
+    assert s.first == 0, f'{s.first} is not 0'
+    digits = f'{s.first}.'
+    s = s.rest
+    for _ in range(k):
+        assert s.first >= 0 and s.first < 10, f'{s.first} is not a digit'
+        digits += str(s.first)
+        s = s.rest
+    print(digits + '...')
+
+def divide(n, d):
+    """Return a linked list with a cycle containing the digits of n/d.
+
+    >>> display(divide(5, 6))
+    0.8333333333...
+    >>> display(divide(2, 7))
+    0.2857142857...
+    >>> display(divide(1, 2500))
+    0.0004000000...
+    >>> display(divide(3, 11))
+    0.2727272727...
+    >>> display(divide(3, 99))
+    0.0303030303...
+    >>> display(divide(2, 31), 50)
+    0.06451612903225806451612903225806451612903225806451...
+    """
+    assert n > 0 and n < d
+    result = Link(0)  # The zero before the decimal point
+    "*** YOUR CODE HERE ***"
+    dict, tail = {}, result
+    while n not in dict:
+        q, r = 10 * n // d, 10 * n % d
+        tail.rest = Link(q)
+        tail = tail.rest
+        dict[n] = tail
+        n = r
+    tail.rest = dict[n]
+            
+    return result
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
