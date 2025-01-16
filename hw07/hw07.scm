6522868a290bfa50ba8951c7119860cb91fcddca
MotingLiuu <moting.liuu@gmail.com>
2025-01-16 21:14:47 +0900

    completed hw07-

--- hw07/hw07.scm
+++ hw07/hw07.scm
@@ -1,15 +1,23 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (if (= 0 exp)
+      1
+      (if (= 0 (modulo exp 2))
+          (square (pow base (/ exp 2)))
+          (* base (pow base (- exp 1)))
+      )
+  )
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cddr s)))
