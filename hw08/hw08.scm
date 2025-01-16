2058d95bf9f9ffe8279f352fa83dbf349a3afee5
MotingLiuu <moting.liuu@gmail.com>
2025-01-17 02:52:36 +0900

    compelted hw08

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,7 +1,39 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    (if (< (length s) 2) 
+        #t
+        (if (> (car s) (car (cdr s)))
+            #f
+            (ascending? (cdr s))
+        )
+    )
+)
+(define (my-filter pred s) 
+    (if (= 0 (length s))
+        s
+    (if (pred (car s))
+        (cons (car s) (my-filter pred (cdr s)))
+        (my-filter pred (cdr s))
+    )
+    )
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (cond 
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (append (list (car lst1) (car lst2)) (interleave (cdr lst1) (cdr lst2))))
+    )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
-
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    (if (<= (length s) 1)
+        s
+        (cons (car s)
+            (no-repeats 
+                (my-filter (lambda (x) (not (= (car s) x)))
+                    (cdr s)
+                )
+            )
+        )
+    )
+)
