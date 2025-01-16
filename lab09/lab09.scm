2c5d7d910d7bc47e6fc503736fe052012b68cb27
MotingLiuu <moting.liuu@gmail.com>
2025-01-16 19:08:11 +0900

    completed lab09

--- lab09/lab09.scm
+++ lab09/lab09.scm
@@ -1,10 +1,19 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond 
+  ((< num1 num2) -1)
+  ((= num1 num2) 0)
+  (else 1)
+))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (define (adder x) (+ x num)) adder)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (define (fg x) (f (g x))) fg)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (define (repeat_n n x)
+    (if (= n 0) x (f (repeat_n (- n 1) x))))
+  (define (func x)
+    (repeat_n n x))
+  func)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +25,8 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (if (= 0 (modulo (max a b) (min a b)))
+      (min a b)
+      (gcd (min a b) (modulo (max a b) (min a b)))
+  ))
